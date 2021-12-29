2a87a1461cbf9468bfaa4bc6f67de1dbfef248b0
Taiwo Adejare <adejareemma@gmail.com>
2021-12-29 08:43:48 +0100

    main dart with size config

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:tesla_app/modules/home/home_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:tesla_app/modules/main/main_screen.dart';
+import 'package:tesla_app/modules/welcome/welcome_screen.dart';
+
+import 'config/size_config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +15,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tesla App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
-    );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return LayoutBuilder(builder: (context, constraints) {
+      return OrientationBuilder(builder: (context, orientation) {
+        SizeConfig().init(constraints, orientation);
+        return MaterialApp(
+          title: 'Tesla App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const WelcomeScreen(),
+          routes: {MainScreen.routeName: (ctx) => const MainScreen()},
+        );
+      });
+    });
   }
 }
