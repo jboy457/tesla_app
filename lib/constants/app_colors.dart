7d9905f40fb81c526936a19374336773d6efdfcf
Taiwo Adejare <adejareemma@gmail.com>
2021-12-29 08:44:25 +0100

    App colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xFF058DD9);
-  static const secondary = Color(0xFF030303);
+  static const secondarylight = Color.fromRGBO(53, 58, 64, 1);
+  static const secondaryDark = Color.fromRGBO(21, 22, 26, 1);
+  static const secondarylight1 = Color(0xFF7F8489);
 }
