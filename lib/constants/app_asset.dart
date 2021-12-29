f4c1049efb1f4130bc5ed834f87e0bc560492af1
Taiwo Adejare <adejareemma@gmail.com>
2021-12-29 08:44:13 +0100

    app assets for images and icons

---
+++ lib/constants/app_asset.dart
@@ -0,0 +1,17 @@
+class AppIcons {
+  static const String setting = 'assets/icons/setting.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String profile = 'assets/icons/profile.svg';
+  static const String more = 'assets/icons/more.svg';
+  static const String cool = 'assets/icons/cool.svg';
+  static const String dry = 'assets/icons/dry.svg';
+  static const String program = 'assets/icons/program.svg';
+  static const String power = 'assets/icons/power.svg';
+}
+
+class AppImages {
+  static const String car = 'assets/images/car.png';
+  static const String car1 = 'assets/images/car1.png';
+  static const String mask = 'assets/images/mask.png';
+  static const String mask1 = 'assets/images/mask1.png';
+}
